81f12eb31d4ed3a64a0068d9042e674a594fe844
Ian Kiguru <iankiguru72@outlook.com>
2024-01-13 16:54:13 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
+"""Lockboxes"""
+
+
 def canUnlockAll(boxes):
     """
-    Determine if all the boxes can be opened.
+    a method that determines if all the boxes can be opened.
 
-    Args:
-        boxes (list): A list of lists where each list represents a box with keys.
-
-    Returns:
-        bool: True if all boxes can be opened, else False.
+    :param boxes:
+    :return: True or False
     """
-    if not boxes or not isinstance(boxes, list):
+    if not boxes or type(boxes) is not list:
         return False
 
-    # Set to keep track of opened boxes
-    opened_boxes = set()
-    # List to keep track of boxes to be checked
-    boxes_to_check = [0]
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and 0 <= key < len(boxes):
+                unlocked.append(key)
 
-    while boxes_to_check:
-        current_box = boxes_to_check.pop()
-
-        # Skip if the box is already opened
-        if current_box in opened_boxes:
-            continue
-
-        # Open the current box and add its keys to boxes_to_check
-        opened_boxes.add(current_box)
-        boxes_to_check.extend(boxes[current_box])
-
-    # Check if all boxes are opened
-    return len(opened_boxes) == len(boxes)
-
+    return len(unlocked) == len(boxes)
